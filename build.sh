2ac1df3e6170a73ca17ee92da2bd893c03dc8b8a
paganellif <filippo1paganelli@gmail.com>
2024-11-19 08:26:41 +0100

    chore: update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "CLEANING OUTPUT BUILD DIR: .pio/build/"
+
+rm -rf .pio/build/
+
 echo "START BUILD"
 
 platformio run -e STM32F103RE_creality ; echo "done" >|"/var/folders/gq/_84s180x0lb_4whxvs2x52nm0000gn/T/ipc"
